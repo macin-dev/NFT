387d1f09039dc586bee83199bfd20622cc48740d
macin <ernesto.dev24@gmail.com>
2024-09-15 10:06:34 -0600

    Replaced jpeg to png image format

--- src/components/homepage/trending/items/users.js
+++ src/components/homepage/trending/items/users.js
@@ -1,29 +1,29 @@
 export const users = {
   collection_1: [
     {
-      url: "./assets/trending/row1.jpeg",
+      url: "./assets/trending/row1.png",
       name: "GoddesEirene",
     },
     {
-      url: "./assets/trending/row2.jpeg",
+      url: "./assets/trending/row2.png",
       name: "Madman",
     },
     {
-      url: "./assets/trending/row3.jpeg",
+      url: "./assets/trending/row3.png",
       name: "Winter Madagascar",
     },
   ],
   collection_2: [
     {
-      url: "./assets/trending/row4.jpeg",
+      url: "./assets/trending/row4.png",
       name: "SpaceExplorer",
     },
     {
-      url: "./assets/trending/row5.jpeg",
+      url: "./assets/trending/row5.png",
       name: "GoddesEirene",
     },
     {
-      url: "./assets/trending/row6.jpeg",
+      url: "./assets/trending/row6.png",
       name: "Madman",
     },
   ],
