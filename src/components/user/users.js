376d02c88a13efb4086fb086763428d462d75abf
macin <ernesto.dev24@gmail.com>
2024-09-25 10:29:00 -0600

    fix users url background

--- src/components/user/users.js
+++ src/components/user/users.js
@@ -1,40 +1,40 @@
 export const users = [
   {
     id: Math.random(),
-    bgURL: "bg-[url('./assets/users/background1.jpeg')]",
-    avatar: "./assets/users/user1.jpeg",
+    bgURL: "bg-[url('.public/assets/users/background1.jpeg')]",
+    avatar: "./public/assets/users/user1.jpeg",
   },
   {
     id: Math.random(),
     bgURL: "bg-[url('./assets/users/background2.jpeg')]",
-    avatar: "./assets/users/user2.jpeg",
+    avatar: "./public/assets/users/user2.jpeg",
   },
   {
     id: Math.random(),
     bgURL: "bg-[url('./assets/users/background3.jpeg')]",
-    avatar: "./assets/users/user3.jpeg",
+    avatar: "./public/assets/users/user3.jpeg",
   },
 
   {
     id: Math.random(),
     bgURL: "bg-[url('./assets/users/background4.jpeg')]",
-    avatar: "./assets/users/user4.jpeg",
+    avatar: "./public/assets/users/user4.jpeg",
   },
 
   {
     id: Math.random(),
     bgURL: "bg-[url('./assets/users/background5.jpeg')]",
-    avatar: "./assets/users/user5.jpeg",
+    avatar: "./public/assets/users/user5.jpeg",
   },
   {
     id: Math.random(),
     bgURL: "bg-[url('./assets/users/background6.jpeg')]",
-    avatar: "./assets/users/user6.jpeg",
+    avatar: "./public/assets/users/user6.jpeg",
   },
 
   {
     id: Math.random(),
     bgURL: "bg-[url('./assets/users/background7.jpeg')]",
-    avatar: "./assets/users/user7.jpeg",
+    avatar: "./public/assets/users/user7.jpeg",
   },
 ];
