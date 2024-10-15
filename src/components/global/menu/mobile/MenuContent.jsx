d7ef6d9e525378a12f638af60e12e2eb68a8b52f
macin <ernesto.dev24@gmail.com>
2024-10-14 23:35:19 -0600

    add menu transition

--- src/components/global/menu/mobile/MenuContent.jsx
+++ src/components/global/menu/mobile/MenuContent.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import { BlackButton_lg } from "../../button";
 import { Theme } from "../Theme";
 import { MobileList } from "./MobileList";
 
 export const MenuContent = () => {
+  const [transition, setTransition] = useState(false);
+
+  useEffect(() => {
+    setTransition(true);
+  }, []);
+
   return (
-    <section className="bg-white flex items-end flex-col py-4 absolute z-30 top-14 right-0 bottom-0 left-0 overflow-auto">
+    <section
+      className={`${
+        transition ? "transition-menu right-0" : ""
+      } w-full bg-white flex items-end flex-col py-4 absolute z-30 top-14 -right-full bottom-0 overflow-auto duration-700`}
+    >
       <div className="flex justify-between px-3 pb-4 self-stretch">
         <BlackButton_lg path="/sing-up" value="Connect Wallet" />
         <Theme />
