e3601e91b64eb26f0e64f555ddce0d6229c3e597
macin <ernesto.dev24@gmail.com>
2024-10-15 11:50:53 -0600

    scroll to top component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Menu } from "./components/global";
 import { Footer } from "./components/global/footer";
+import { ScrollToTop } from "./helper/ScrollToTop";
 
 import "./index.css";
 import { Outlet } from "react-router-dom";
@@ -7,6 +8,7 @@ import { Outlet } from "react-router-dom";
 export function App() {
   return (
     <>
+      <ScrollToTop />
       <Menu />
       <Outlet />
       <Footer />
